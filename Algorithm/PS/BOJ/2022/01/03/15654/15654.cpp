97d626280d3b1d8d969462f8deaf8f4d57f4da0b
chanos-dev <chanos.dev@gmail.com>
2022-01-03 00:26:07 +0900

    22-0103

---
+++ Algorithm/PS/BOJ/2022/01/03/15654/15654.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n, m;
+vector<int> vec;
+
+int arr[9];
+bool used[9];
+
+void nm(int k)
+{
+    if (k == m)
+    {
+        for(int i=0; i<m; i++)
+        {
+            cout<<arr[i]<<" ";
+        }
+        cout<<"\n";
+        return;
+    }
+
+    for(int i=0; i<n; i++)
+    {
+        if (!used[i])
+        {
+            arr[k] = vec[i];
+            used[i] = true;
+            nm(k+1);
+            used[i] = false;
+        }
+    }
+}
+
+int main()
+{
+    cin>>n>>m;
+    for(int i=0; i<n; i++)
+    {
+        int v; cin>>v;
+        vec.push_back(v);
+    }
+    sort(vec.begin(), vec.end());
+    nm(0);
+}
---
+++ Algorithm/PS/BOJ/2022/01/03/15655/15655.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int n, m;
+vector<int> vec;
+
+int arr[9];
+bool used[9];
+
+void nm(int k)
+{
+    if (k == m)
+    {
+        for(int i=0; i<m; i++)
+        {
+            cout<<arr[i]<<" ";
+        }
+        cout<<"\n";
+        return;
+    }
+
+    for(int i=0; i<n; i++)
+    {
+        arr[k] = vec[i];
+        nm(k+1);
+    }
+}
+
+int main()
+{
+    cin>>n>>m;
+    for(int i=0; i<n; i++)
+    {
+        int v; cin>>v;
+        vec.push_back(v);
+    }
+    sort(vec.begin(), vec.end());
+    nm(0);
+}
