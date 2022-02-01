d2739e6fd541b9128ee9ac453122d52c1c0a2805
chanos-dev <chanos.dev@gmail.com>
2022-02-01 20:36:45 +0900

    22-0201

---
+++ Algorithm/PS/BOJ/2022/02/01_csharp/01/14267/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14267
+{
+    class Program
+    {
+        private static int[] Person = new int[100001];
+        private static List<int>[] ScorePerson = new List<int>[100001];
+
+        private static void Init(int n)
+        {
+            for(int i=0; i<n+1; i++)
+                ScorePerson[i] = new List<int>();
+        }
+
+        private static void Set(string[] person)
+        {
+            for(int i=0; i<person.Length; i++)
+            {
+                if (person[i] == "-1") continue;
+                ScorePerson[int.Parse(person[i])].Add(i+1);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int n, m;
+
+            var input = Console.ReadLine().Split(' ');
+
+            n = int.Parse(input[0]);
+            m = int.Parse(input[1]);
+
+            Init(n);
+
+            var person = Console.ReadLine().Split(' ');
+            Set(person);
+
+            for (int i=0; i<m; i++)
+            {
+                var w = Console.ReadLine().Split(' ');
+                int p = int.Parse(w[0]);
+                int k = int.Parse(w[1]);
+                Person[p] += k;
+            }
+
+            Score(1);
+
+            for(int i=1; i<=n; i++)
+            {
+                Console.Write($"{Person[i]} ");
+            }
+        }
+
+        private static void Score(int s)
+        {
+            if (ScorePerson[s] is null) return;
+
+            foreach(var i in ScorePerson[s])
+            {
+                Person[i] += Person[s];
+                Score(i);
+            }
+        }
+    }
+}
---
+++ Algorithm/PS/BOJ/2022/02/01_csharp/01/16197/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace _16197
+{
+    class Program
+    {
+        private static int n;
+        private static int m;
+        private static int ans = 9999999;
+
+        private static int[] dx = new[] { 1, 0, -1, 0 };
+        private static int[] dy = new[] { 0, 1, 0, -1 };
+
+        private static Point[] Pos = new Point[2];
+
+        private static char[,] board = new char[21, 21];
+        static void Main(string[] args)
+        {
+            var inputItems = Console.ReadLine().Split(' ');
+
+            n = int.Parse(inputItems[0]);
+            m = int.Parse(inputItems[1]);
+
+            int idx = 0;
+            for (int i=0; i<n; i++)
+            {
+                var line = Console.ReadLine();
+                for(int j=0; j<line.Length; j++)
+                {
+                    board[i, j] = line[j];
+                    if (board[i, j] == 'o')
+                        Pos[idx++] = (new Point(i, j));
+                }
+            }
+
+            Solution();
+
+            if (ans > 10)
+                Console.Write("-1");
+            else
+                Console.Write(ans);
+        }
+
+        private static void Solution()
+        {
+            for(int d=0; d<4; d++)
+            {
+                Move(Pos, d, 1);
+            }
+        }
+
+        private static void Move(Point[] pos, int d, int k)
+        {
+            if (k > 10)
+                return;
+            if (k > ans)
+                return;
+
+            Point[] temp = new Point[2];
+
+            for(int i=0; i<temp.Length; i++)
+            {
+                temp[i] = new Point(pos[i].X + dx[d], pos[i].Y + dy[d]);
+            } 
+
+            if (CheckDroppedBall(temp) == 2)
+                return;
+            else if (CheckDroppedBall(temp) == 1)
+            {
+                if (ans > k)
+                    ans = k;
+                return;
+            }
+
+            for (int i = 0; i < temp.Length; i++)
+            {
+                if (board[temp[i].X, temp[i].Y] == '#')
+                    temp[i] = pos[i];
+            }
+
+            bool isSamePos = false;
+            for (int i = 0; i < temp.Length - 1; i++)
+            {
+                if (temp[i].Equals(temp[i + 1]))
+                    isSamePos = true;
+            }
+
+            if (isSamePos)
+                return;
+
+            for (int m=0; m<4; m++)
+            {
+                Move(temp, m, k + 1);
+            }
+        }
+
+        private static int CheckDroppedBall(Point[] pos)
+        {
+            int o = 0;
+
+            foreach(var p in pos)
+            {
+                if (p.X < 0 || p.X >= n || p.Y < 0 || p.Y >= m)
+                    o++;
+            }
+
+            return o;
+        }
+    }
+
+    struct Point
+    {
+        public int X;
+        public int Y;
+
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+}
